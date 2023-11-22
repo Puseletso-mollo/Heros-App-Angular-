345a617ecb1d4d7445505e913af013ad794724ac
Puseletso-mollo <puseletsomollo26@gmail.com>
2023-11-22 11:35:00 +0200

    Tour of Heroes App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroesComponent,
+    
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    FormsModule,
+    HeroDetailComponent,
+    
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
